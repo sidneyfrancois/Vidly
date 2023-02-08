3ef657bd0049230df4866cad6948130d557c1608
Sidney François <sidney.fvm.ps@outlook.com>
2023-02-07 22:33:08 -0300

    Add MemberShipType, add relation between MemberShipType and Customer

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,5 +9,9 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+        public MembershipType MyProperty { get; set; }
     }
 }

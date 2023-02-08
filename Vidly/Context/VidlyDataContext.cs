660189cb405b0696ae37f3bb8fe298fa76ac02d5
Sidney François <sidney.fvm.ps@outlook.com>
2023-02-07 21:47:42 -0300

    Add connection to the database with Entity Framework

---
+++ Vidly/Context/VidlyDataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Context
+{
+    public class VidlyDataContext : DbContext
+    {
+        public VidlyDataContext()
+            :base("VidlyConnectionString")
+        {}
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}

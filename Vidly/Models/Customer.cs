bac6cfcc83c995ab860dfbb825d6ea9d71e29669
Sidney François <sidney.fvm.dev@outlook.com>
2023-02-14 18:21:47 -0300

    Add Validation using the data annotation and ModelState. Add validation message into the View.

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,6 +9,9 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
